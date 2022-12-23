c3b3814d1f9bb8e7af3aaf8dcb9db299ccd9ce3d
Dominique <dominique.del1801@alumnos.ubiobio.cl>
2022-12-23 20:24:30 -0300

    gg

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "https://proyectotitulo.herokuapp.com";
+export const BASE_URL = "https://proyectotitulo.herokuapp.com/v1";
 
 const api = axios.create({
   baseURL: BASE_URL,
