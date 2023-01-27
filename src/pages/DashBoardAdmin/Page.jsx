10dd2838a7e6739453f1316e60f894b2ce554db0
Dominique <dominique.del1801@alumnos.ubiobio.cl>
2023-01-27 10:30:46 -0300

    typo

--- src/pages/DashBoardAdmin/Page.jsx
+++ src/pages/DashBoardAdmin/Page.jsx
@@ -57,9 +57,14 @@ const Page = ({
                   dense={true}
                 >
                   <Typography variant="h6" component="p">
-                    {`${data.name}  ${data.lastname} - ${
+                    {`${
                       data.rol === 1 ? "Profesor" : "Estudiante"
                     }`}
+                  </Typography>
+                  <Typography variant="h6" component="p">
+                    {`${data.name}  ${data.lastname} 
+                      
+                    `}
                   </Typography>
                 </ListItemButton>
                 <ButtonGroup
